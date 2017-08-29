2b2a3b7b5c5cd5e8c88fca7f49d1d024c204595e
danielsan <danielizquierdofernandez89@gmail.com>
2017-08-29 17:49:41 +0100

    I've just uploaded project01 (no random CPU moves)

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,123 @@
-console.log('test 1 2 3');
+$(()=>{
+
+  const $player = $('#player');
+  const $cpu= $('#cpu');
+  const $gameScreen= $('#mainScreen');
+  //const $playerLife = $('#playerLife');
+  let xTop = $gameScreen.offset().top, yLeft = $gameScreen.offset().left;
+  let yRight = yLeft + $gameScreen.outerWidth();
+  let xBottom = xTop +$gameScreen.outerHeight();
+  console.log(xTop,yLeft,yRight,xBottom);
+  //let superAttack=0;
+
+
+
+  function getPositions(){
+    const playerRight = $player.offset().left+$player.outerWidth();
+    const playerLeft = $player.offset().left;
+    const playerTop = $player.offset().top;
+    const playerBottom = $player.offset().top+$player.outerHeight();
+    const cpuRight = $player.offset().left+$player.outerWidth();
+    const cpuLeft = $cpu.offset().left;
+    const cpuTop = $cpu.offset().top;
+    const cpuBottom = $cpu.offset().top+$cpu.outerHeight();
+
+    return playerRight,playerLeft,playerTop,playerBottom,cpuRight,cpuLeft,cpuTop,cpuBottom;
+  }
+
+  function collisionCPU(){
+    getPositions();
+    if(cpuRight < playerLeft){
+      console.log('no collision');
+    }else{
+      if(cpuLeft < playerRight && playerTop<cpuBottom){
+        console.log('collision');
+      }
+    }
+  }
+
+  function collisionPlayer(){
+    getPositions();
+    if($player.offset().left+$player.outerWidth()>=$cpu.offset().left){
+      console.log('collision');
+    }
+  }
+  
+  function cpuMoves() {
+    setInterval(() => {
+      $cpu.animate({
+        left: '-=20%'
+      }, {
+        duration: 200,
+        progress: collisionCPU,
+        complete() {
+          $cpu.animate({ left: '+=20%' }, 100);
+        }
+      });
+
+    },5000);
+  }
+
+  function playerMoves(event) {
+    getPositions();
+    switch (event.which) {
+      case 32:
+        if($player.hasClass('horizontal')){
+          $player.animate({left: '-=30%'}, 90, collisionPlayer);
+          break;
+        }
+        $player.animate({left: '+=30%'}, 90);
+        break;
+      case 37:
+        if(($player.offset().left-$player.offset().left*0.01)>=yLeft && $player.hasClass('horizontal')){
+          $player.animate({left: '-=10%'}, 1);
+          break;
+        }else if(($player.offset().left-$player.offset().left*0.01)>=yLeft && !$player.hasClass('horizontal')){
+          $player.animate({left: '-=10%'}, 1);
+          $player.toggleClass('horizontal');
+          break;
+        }
+        break;
+      case 39:
+        if(($player.offset().left+($player.offset().left)*0.01)>=yRight){
+          break;
+        }else if(($player.offset().left+($player.offset().left)*0.05)<yRight+$player.outerWidth() && $player.hasClass('horizontal')){
+          $player.toggleClass('horizontal');
+          $player.animate({left: '+=10%'}, 1);
+          break;
+        }
+        $player.animate({left: '+=10%'}, 1);
+        break;
+      case 38:
+        if(($player.offset().top-$player.offset().top*0.01)<=xBottom*0.5){
+          break;
+        }
+        $player.animate({top: '-=10%'}, 1);
+        break;
+      case 40:
+        if(($player.offset().top-$player.offset().top*0.01)>=xBottom-$player.outerHeight()){
+          break;
+        }
+        $player.animate({top: '+=10%'}, 1);
+        break;
+    }
+  }
+
+  function resizeWindow(){
+    $(window).resize(function(){
+      xTop = $gameScreen.offset().top;
+      yLeft = $gameScreen.offset().left;
+      yRight = (yLeft + $gameScreen.outerWidth());
+      xBottom = (xTop +$gameScreen.outerHeight());
+      console.log(xTop,yLeft,yRight,xBottom);
+    });
+  }
+
+  function play(){
+    resizeWindow();
+    $(window).keydown(playerMoves);
+    cpuMoves();
+  }
+  play();
+
+});
