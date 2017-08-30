5848c47cd5d8b4288103fdbeabf6e07af948f612
danielsan <danielizquierdofernandez89@gmail.com>
2017-08-30 17:01:55 +0100

    I've just uploaded project01 (Collision checked and ranges of motion)

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,12 @@
 $(()=>{
 
   const $player = $('#player');
-  const $cpu= $('#cpu');
+  const $cpu= $('#boss');
+  const $dactyl =$('#dactyl');
   const $gameScreen= $('#mainScreen');
+  const $specialAttack = $('#specialAttack');
+  const $life = $('#life');
+
   //const $playerLife = $('#playerLife');
   const cpuMove={
     left: 'left',
@@ -10,53 +14,64 @@ $(()=>{
     top: 'top',
     bottom: 'bottom'
   };
-  let xTop = $gameScreen.offset().top, yLeft = $gameScreen.offset().left;
-  let yRight = yLeft + $gameScreen.outerWidth();
-  let xBottom = xTop +$gameScreen.outerHeight();
-  console.log(xTop,yLeft,yRight,xBottom);
-  let superAttack=0;
+  const cpuDactylMove={
+    1: '0%',
+    2: '15%',
+    3: '30%',
+    4: '45%',
+    5: '60%',
+    6: '75%'
+  };
 
-  function getPositions(){
-    const playerRight = $player.offset().left+$player.outerWidth();
-    const playerLeft = $player.offset().left;
-    const playerTop = $player.offset().top;
-    const playerBottom = $player.offset().top+$player.outerHeight();
-    const cpuRight = $player.offset().left+$player.outerWidth();
-    const cpuLeft = $cpu.offset().left;
-    const cpuTop = $cpu.offset().top;
-    const cpuBottom = $cpu.offset().top+$cpu.outerHeight();
+  const hit=[];
+  let gameSpeed = 1;
+  let gameScreenTop = $gameScreen.offset().top;
+  let gameScreenLeft = $gameScreen.offset().left;
+  let gameScreenRight = gameScreenLeft + $gameScreen.outerWidth(true);
+  let gameScreenBottom = gameScreenTop +$gameScreen.outerHeight(true);
 
-    return playerRight,playerLeft,playerTop,playerBottom,cpuRight,cpuLeft,cpuTop,cpuBottom;
-  }
+  // function getPositions(){
+  //   let playerRight = $player.outerWidth(true);
+  //   let playerLeft = $player.offset().left;
+  //   let playerTop = $player.offset().top;
+  //   let playerBottom = $player.outerHeight(true);
+  //   let cpuRight = $player.offset().left+$player.outerWidth(true);
+  //   let cpuLeft = $cpu.offset().left;
+  //   let cpuTop = $cpu.offset().top;
+  //   let cpuBottom = $cpu.outerHeight(true);
+  //
+  //   return {
+  //     playerRight,playerLeft,playerTop,playerBottom,cpuRight,cpuLeft,cpuTop,cpuBottom
+  //   };
+  // }
 
-  function collisionCPU(playerRight,playerLeft,playerTop,playerBottom,cpuRight,cpuLeft,cpuTop,cpuBottom){
+  // function collision() {
+  //   const positions=getPositions();
+  //   var b1 = positions.playerTop + positions.playerButtom;
+  //   var r1 = positions.playerLeft + positions.playerRight;
+  //   var b2 = positions.cpuTop + positions.cpuBottom;
+  //   var r2 = positions.cpuLeft + positions.cpuRight;
+  //
+  //   if (b1 < positions.cpuTop || positions.playerTop > b2 || r1 < positions.cpuLeft || positions.playerLeft > r2) return false;
+  //   return true;
+  // }
 
-    if(cpuRight < playerLeft){
-      console.log('no collision');
-    }else{
-      if(cpuLeft < playerRight && playerTop<cpuBottom){
-        console.log('collision');
-      }
-    }
-  }
+  function collision($player, $cpu) {
+    var x1 = $player.offset().left;
+    var y1 = $player.offset().top;
+    var h1 = $player.outerHeight(true);
+    var w1 = $player.outerWidth(true);
+    var b1 = y1 + h1;
+    var r1 = x1 + w1;
+    var x2 = $cpu.offset().left;
+    var y2 = $cpu.offset().top;
+    var h2 = $cpu.outerHeight(true);
+    var w2 = $cpu.outerWidth(true);
+    var b2 = y2 + h2;
+    var r2 = x2 + w2;
 
-  function collisionPlayer(){
-    getPositions();
-    if($player.offset().left+$player.outerWidth()>=$cpu.offset().left){
-      console.log('collision');
-    }
-  }
-
-  function cpuSpeedAttack(){
-    return $cpu.animate({
-      left: '-=20%'
-    }, {
-      duration: 200,
-      progress: collisionCPU(getPositions()),
-      complete() {
-        $cpu.animate({ left: '+=20%' }, 100);
-      }
-    });
+    if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
+    return true;
   }
 
   function cpuMoves() {
@@ -71,67 +86,152 @@ $(()=>{
   function startCPUMovement(){
     setInterval( ()=>{
       console.log(cpuMoves());
-      superAttack++;
-      if(superAttack>5){
-        cpuSpeedAttack();
-        superAttack=0;
-        console.log(superAttack);
+      const speedAttack=1 + Math.floor(Math.random() * 6);
+      if(speedAttack===3){
+        cpuDash();
       }
-    },1000);
+    },1000/gameSpeed);
   }
 
+  function cpuDash(){
+    $cpu.animate({
+      left: '-=20%'
+    }, {
+      duration: 1000/gameSpeed,
+      progress: () => console.log(collision($cpu,$player)),
+      complete() {
+        $cpu.animate({ left: '+=20%' }, 100);
+      }
+    });
+    cpuDactylAttack();
+  }
+
+  function cpuDactylAttack(){
+    const dactylPosition = 1 + Math.floor(Math.random() * 6);
+    $dactyl.toggleClass('hidden');
+    $dactyl.animate({
+      top: '+=50%'
+    }, {
+      duration: 1000/gameSpeed,
+      progress: () => checkCollisionDactyl(),
+      complete() {
+        $dactyl.toggle( 'bounce', { times: 5 }, 'slow', () => {
+          $dactyl.css({top: '30%', left: cpuDactylMove[dactylPosition]});
+        });
+      }
+    });
+  }
+
+  function checkCollisionDactyl(){
+
+    hit.push(collision($dactyl,$player));
+    console.log(hit);
+    // if(true in hit)){
+    //   $('.heart:last-child', this).remove();
+    //   return false;
+    // }
+  }
+
+
   function playerMoves(event) {
-    getPositions();
+
     switch (event.which) {
+      case 17:
+        playerSpecialAttack();
+        break;
       case 32:
-        if($player.hasClass('horizontal')){
-          $player.animate({left: '-=30%'}, 90, collisionPlayer);
-          break;
-        }
-        $player.animate({left: '+=30%'}, 90);
+        playerDash();
         break;
       case 37:
-        if(($player.offset().left-$player.offset().left*0.01)>=yLeft && $player.hasClass('horizontal')){
-          $player.animate({left: '-=10%'}, 1);
-          break;
-        }else if(($player.offset().left-$player.offset().left*0.01)>=yLeft && !$player.hasClass('horizontal')){
-          $player.animate({left: '-=10%'}, 1);
-          $player.toggleClass('horizontal');
-          break;
-        }
+        playerLeftMove();
         break;
       case 39:
-        if(($player.offset().left+($player.offset().left)*0.01)>=yRight){
-          break;
-        }else if(($player.offset().left+($player.offset().left)*0.05)<yRight+$player.outerWidth() && $player.hasClass('horizontal')){
-          $player.toggleClass('horizontal');
-          $player.animate({left: '+=10%'}, 1);
-          break;
-        }
-        $player.animate({left: '+=10%'}, 1);
+        playerRightMove();
         break;
       case 38:
-        if(($player.offset().top-$player.offset().top*0.01)<=xBottom*0.5){
-          break;
-        }
-        $player.animate({top: '-=10%'}, 1);
+        playerUpMove();
         break;
       case 40:
-        if(($player.offset().top-$player.offset().top*0.01)>=xBottom-$player.outerHeight()){
-          break;
-        }
-        $player.animate({top: '+=10%'}, 1);
+        playerDownMove();
         break;
     }
   }
 
+  function playerSpecialAttack(){
+    const duration = setInterval(playerSpecialAttackEffects ,1000);
+    setTimeout(function() {
+      clearInterval( duration );
+    }, 2000);
+
+  }
+
+  function playerSpecialAttackEffects(){
+
+    $player.toggleClass('playerSpecialAttack');
+    $specialAttack.toggleClass('hidden');
+  }
+
+  function playerDash(){
+    if($player.hasClass('mirror')){
+      $player.animate({
+        left: '-=20%'
+      }, {
+        duration: 90,
+        progress: () => collision($player,$cpu)
+      });
+      return false;
+    }
+    $player.animate({
+      left: '+=20%'
+    }, {
+      duration: 90,
+      progress: () => collision($player,$cpu)
+    });
+  }
+
+  function playerLeftMove(){
+    if(($player.offset().left-$player.offset().left*0.01)>=gameScreenLeft && $player.hasClass('mirror')){
+      $player.animate({left: '-=2%'}, 1);
+      return false;
+    }else if(($player.offset().left-$player.offset().left*0.01)>=gameScreenLeft && !$player.hasClass('mirror')){
+      $player.animate({left: '-=2%'}, 1);
+      $player.toggleClass('mirror');
+      return false;
+    }
+  }
+
+  function playerRightMove(){
+    if(($player.offset().left+($player.offset().left)*0.01)>=gameScreenRight){
+      return false;
+    }else if(($player.offset().left+($player.offset().left)*0.05)<gameScreenRight+$player.outerWidth() && $player.hasClass('mirror')){
+      $player.toggleClass('mirror');
+      $player.animate({left: '+=2%'}, 1);
+      return false;
+    }
+    $player.animate({left: '+=2%'}, 1);
+  }
+
+  function playerUpMove(){
+    if(($player.offset().top-$player.offset().top*0.01)<=gameScreenBottom*0.5){
+      return false;
+    }
+    $player.animate({top: '-=2%'}, 1);
+  }
+
+  function playerDownMove(){
+    if(($player.offset().top-$player.offset().top*0.01)>=gameScreenBottom-$player.outerHeight()){
+      return false;
+    }
+    $player.animate({top: '+=2%'}, 1);
+  }
+
   function resizeWindow(){
     $(window).resize(function(){
-      xTop = $gameScreen.offset().top;
-      yLeft = $gameScreen.offset().left;
-      yRight = (yLeft + $gameScreen.outerWidth());
-      xBottom = (xTop +$gameScreen.outerHeight());
-      console.log(xTop,yLeft,yRight,xBottom);
+      gameScreenTop = $gameScreen.offset().top;
+      gameScreenLeft = $gameScreen.offset().left;
+      gameScreenRight = (gameScreenLeft + $gameScreen.outerWidth());
+      gameScreenBottom = (gameScreenTop +$gameScreen.outerHeight());
+      console.log(gameScreenTop,gameScreenLeft,gameScreenRight,gameScreenBottom);
     });
   }
 
